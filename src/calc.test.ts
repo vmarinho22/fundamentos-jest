cece8c953c81d4dff980cb12867039bc4373ad82
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-01-12 14:59:18 -0300

    feat(test): adding more tests to calc

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,11 +1,24 @@
-import { add } from '../src/calc';
+import { add, multiply } from '../src/calc';
 
 describe("Test Calc", () => {
-  it('should be add two numbers - 5 + 10', () => {
-    expect(add(5,10)).toBe(15);
-  })
 
-  it('should be add two numbers - 45 + 105', () => {
-    expect(add(45,105)).toBe(150);
+  describe("Method add", () => {
+    it('should be add two numbers: 5 + 10', () => {
+      expect(add(5,10)).toBe(15);
+    })
+  
+    it('should be add two numbers: 45 + 105', () => {
+      expect(add(45,105)).toBe(150);
+    })
+  });
+
+  describe("Method multiply", () => {
+    it('should be multiply two numbers: 5 * 10', () => {
+      expect(multiply(5,10)).toBe(50);
+    })
+  
+    it('should be multiply numbers: 9 * 20', () => {
+      expect(multiply(9,20)).toBe(180);
+    })
   })
 })
--- src/calc.ts
+++ src/calc.ts
@@ -1,6 +1,8 @@
 const add = (a: number, b: number): number => a + b;
+const multiply = (a: number, b: number): number => a * b;
 
 
 export {
-  add
+  add,
+  multiply
 }
